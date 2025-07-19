05213a87ddd572fdadf5dc914b1aef4ab955407b
Abhi Patel <94160117+Abhipatel18@users.noreply.github.com>
2025-07-19 14:10:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from CI/CD!'));
+app.get('/', (req, res) => res.send('Hello from Abhi I successfully deployed application on the cloud platform'));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
