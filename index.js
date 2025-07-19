a7f5852a4d4b05d8e5f128c5382cb95a2c6b7b85
Abhi Patel <abhippateltx@gmail.com>
2025-07-19 14:25:54 -0400

    tail

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from CI/CD!'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
